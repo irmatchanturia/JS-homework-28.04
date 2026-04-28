1a2639d57b4b847f11bdd80a297f3d2f49681963
IrmaTchanturia <irmachanturia1@gmail.com>
2026-04-28 12:35:35 +0400

    first JS homework

---
+++ script.js
@@ -0,0 +1,92 @@
+// .................................პირველი დავალება.........................//
+
+// let firstNum = Number(prompt("მომწერე პირველი რიცხვი."))
+// var secondNum = Number(prompt("მომწერე მეორე რიცხვი."))
+// const thirdNum = Number(prompt("მომწერე მესამე რიცხვი"))
+
+// let sum = firstNum + secondNum + thirdNum
+// let diff = firstNum - thirdNum
+// let mult = secondNum * firstNum
+// let mod = firstNum % secondNum
+
+
+// console.log(sum)
+// console.log(diff)
+// console.log(mult)
+// console.log(mod)
+// console.log(secondNum++)
+// console.log(secondNum--)
+
+// console.log(sum > diff)
+// console.log(sum < diff)
+// console.log(sum==diff)
+// console.log(sum === diff)
+// console.log(sum !== diff)
+// console.log(mult >= mod)
+// console.log(mult <= mod)
+// console.log(typeof firstNum)
+
+// document.write("sum is more then diff")
+
+
+// ......................მეორე დავალება.............................//
+
+// const myString ="string"
+// const myNumber = 5
+// const myBoolean = true
+// const myUndefined = undefined
+// const myNull = null
+// const myNan = NaN
+// const myArray = [2, 4, 6, 8, 10]
+
+// const person = {
+//     name: "Oto",
+//     age: 29
+// }
+
+// function functionName(){
+//     console.log("String")
+// }
+// console.log(myString==myNan)
+// console.warn(myUndefined===myString)
+// console.error(myNumber<=myBoolean)
+// console.log(typeof myString)
+// console.log(typeof myNumber)
+// console.log(typeof myBoolean)
+// console.log(typeof myUndefined)
+// console.log(typeof myNull)
+// console.log(typeof myNan)
+// console.log(typeof myArray)
+
+//....................მესამე დავალება................//
+
+// let randomNumFirst = Number(prompt("მომწერე რიცხვი"))
+// let randomNumSecond = Number(prompt("მომწერე რიცხვი"))
+// let randomNumThird = Number(prompt("მომწერე რიცხვი"))
+
+// let sumRandom = randomNumFirst + randomNumSecond - randomNumThird
+// let sumRandomSec = randomNumSecond - randomNumThird + randomNumFirst
+
+// console.log(sumRandom > sumRandomSec)
+// document.write(sumRandomSec === sumRandom)
+
+
+//..................მეოთხე დავალება.................//
+
+// let testPrompt = prompt("ეს არის სატესტო პრომპტი")
+// confirm("ეს არის სატესტო ქონფირმი")
+// alert("ეს არის სატესტო ალერტი")
+// console.log(typeof testPrompt)
+// document.write(testPrompt)
+
+// ..........................მეხუთე დავალება..................//
+// let promptTest = Number(prompt("შენი საყვარელი რიცხვი"))
+// let promptTestSec = Number(prompt("შენი მეორე საყვარელი რიცხვი"))
+// let promptSum = promptTest + promptTestSec;
+// let promptDiff = promptTest - promptTestSec;
+// console.log(promptTest + promptTestSec);
+// console.error(promptSum);
+// console.warn(promptDiff);
+// console.log(typeof promptTest);
+// document.write(promptDiff);
+//კომენტარი
